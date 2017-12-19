394e8e5e967ce5a4d2c0f3b474f0b30e6c939d0d
pcharuthamrong <21190577+pcharuthamrong@users.noreply.github.com>
2017-12-19 23:13:27 +0700

    add sendWatered, current preference alert
    sendWatered is for signaling if plant should be watered

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var table = document.querySelector('.plantStatusTable');
 
 var rowCount = 0;
 
+var currentPref = document.querySelector('.currentPref');
 var changeSetting = document.querySelector('.changeSetting');
 var minHumid = document.querySelector('.minHumid');
 var maxHumid = document.querySelector('.maxHumid');
@@ -10,12 +11,24 @@ var maxTemp = document.querySelector('.maxTemp');
 var currentDP = -1;
 
 changeSetting.addEventListener('click', setValues);
+currentPref.addEventListener('click', showPref); 
+
+function showPref(){
+	var minH, maxH, minT, maxT;
+	minH = localStorage.getItem('minHumid');
+	maxH = localStorage.getItem('maxHumid');
+	minT = localStorage.getItem('minTemp');
+	maxT = localStorage.getItem('maxTemp');
+	alert('Current preference is:\n' + 
+		'Humidity: ' + minH + ' - ' + maxH + '\n' + 
+		'Temperature: ' + minT + ' - ' + maxT);
+}
 
 function setValues() {
-	var minH = minHumid.value;
-	var maxH = maxHumid.value;
-	var minT = minTemp.value;
-	var maxT = maxTemp.value;
+	var minH = Number(minHumid.value);
+	var maxH = Number(maxHumid.value);
+	var minT = Number(minTemp.value);
+	var maxT = Number(maxTemp.value);
 	
 	if(minH < 0 || minH > 4095 || maxH < 0 || maxH > 4095 ||
 		minT < 0 || minT > 100 || maxT < 0 || maxT > 100 ||
@@ -78,29 +91,57 @@ function backgroundColor(low, high, recLow, recHigh, current) {
 }
 
 function getData(url) {
-	var xhr= new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 	xhr.open('GET', url, true);
 	
 	xhr.onreadystatechange = function() {
-		if(xhr.readyState == 4) {
+		if(xhr.readyState == 4 && xhr.status == 200) {
 			var status = JSON.parse(xhr.responseText);
 			//alert(xhr.responseText);
 			
+			if(status.feeds[0].field1 == null ||
+			status.feeds[0].field2 == null) {
+				return;
+			}
+			
 			var hValue = Number(status.feeds[0].field1);
 			var date = new Date(status.feeds[0].created_at);
-			var temp2 = Math.floor(Math.random() * 50) + 1;
+			var tValue = Number(status.feeds[0].field2);
 			
-			if(currentDP === -1 || currentDP.getTime() !== date.getTime()) {
-				appendPlantStatus(hValue,temp2,date);
-				if(rowCount > 5) {
-					removeLastRow();
-				}
+			if(currentDP != -1 && currentDP.getTime() == date.getTime()) {
+				return;
+			}
+				
+			appendPlantStatus(hValue,tValue,date);
+			if(rowCount > 5) {
+				removeLastRow();
 			}
 			currentDP = date;
+			
+			var threshold = localStorage.getItem('minHumid')
+			if(threshold != null && hValue < threshold) {
+				sendWatered(1, hValue, tValue);
+			}
 		}
 	}
 	
 	xhr.send();
+}
+
+function sendWatered(value, f1, f2) {
+	var url = 'https://api.thingspeak.com/update?api_key=I4FUQAC7QSAXN7UR'
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', url, true);
+	
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState == 4 && xhr.status == 200) {
+			if(value == 1) {
+				alert('Your plant is watered.');
+			}
+		}
+	}
+	xhr.send('field3=' + value + '&field1=' + f1 + '&field2=' + f2);
 }
 
 getData('https://api.thingspeak.com/channels/368328/feeds.json?api_key=KG5Q1Q7LHIIHIMF3&results=1');
